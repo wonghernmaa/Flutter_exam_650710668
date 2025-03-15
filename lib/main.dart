504f96c0e69c0a677a8666acdadcdd8fb0fe9ea3
wonghernmaa <thepon4444@gmail.com>
2025-03-15 09:54:49 +0700

    ready_for_exam

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:final_exam_650710668/widget/profile_card.dart'; // นำเข้าไฟล์ ProfileCard ถ้าแยกไฟล์ไว้
+import 'package:final_exam_650710668/widget/profile_card.dart';
 
 void main() {
   runApp(const MyApp());
@@ -43,17 +43,69 @@ class ProfileScreen extends StatelessWidget {
             end: Alignment.bottomRight,
           ),
         ),
-        child: Center(
-          child: ProfileCard(
-            name: 'Krissada Khongkaew',
-            position: 'Professor',
-            email: 'thepon4444@egmail.com',
-            phoneNumber: '098-746-3800',
-            imageUrl:
-                'https://scontent.fbkk13-2.fna.fbcdn.net/v/t39.30808-6/481768540_122144629076502717_5660007276847633558_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=dKAhcV_w1RIQ7kNvgFnIzgp&_nc_oc=AdhqFXRFRWAtp1vsQ_RMzaYfHROtliqWj39D8BMnr-qRFEsg36FaWJd7FNrRIoIW1YQ&_nc_zt=23&_nc_ht=scontent.fbkk13-2.fna&_nc_gid=H4zL3_gF1b5ILvxSsrIpEQ&oh=00_AYEzIwUU2cNlR6N6O1xBFPNOsU93T_9f15ijt5NKvK8d7w&oe=67DAA35E',
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ProfileCard(
+              name: 'Krissada Khongkaew',
+              position: 'Professor',
+              email: 'thepon4444@egmail.com',
+              phoneNumber: '098-746-3800',
+              imageUrl:
+                  'https://scontent.fbkk13-2.fna.fbcdn.net/v/t39.30808-6/481768540_122144629076502717_5660007276847633558_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=dKAhcV_w1RIQ7kNvgFnIzgp&_nc_oc=AdhqFXRFRWAtp1vsQ_RMzaYfHROtliqWj39D8BMnr-qRFEsg36FaWJd7FNrRIoIW1YQ&_nc_zt=23&_nc_ht=scontent.fbkk13-2.fna&_nc_gid=H4zL3_gF1b5ILvxSsrIpEQ&oh=00_AYEzIwUU2cNlR6N6O1xBFPNOsU93T_9f15ijt5NKvK8d7w&oe=67DAA35E',
+            ),
+            const SizedBox(height: 20),
+            _buildStatBar('HP', 0.90, Colors.red, Colors.redAccent),
+            const SizedBox(height: 10),
+            _buildStatBar('Mana', 0.5, Colors.blue, Colors.lightBlueAccent),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildStatBar(
+      String label, double value, Color startColor, Color endColor) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '$label: ${(value * 100).toInt()}%',
+          style:
+              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 5),
+        Stack(
+          children: [
+            // แถบสีพื้นหลัง
+            Container(
+              height: 20,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade800,
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            // แถบ Progress ที่เป็น Gradient
+            FractionallySizedBox(
+              widthFactor: value, // ปรับขนาดตามค่าที่กำหนด (0.75 = 75%)
+              child: Container(
+                height: 20,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  gradient: LinearGradient(colors: [startColor, endColor]),
+                  boxShadow: [
+                    BoxShadow(
+                      color: startColor.withOpacity(0.5),
+                      blurRadius: 4,
+                      offset: const Offset(2, 2),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
